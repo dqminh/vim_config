fb9792396740d108279b4f927c1a2c4a642f8869
Dao Quang Minh <dqminh89@gmail.com>
2010-07-18 14:07:59 +0800

    fix php 5.3

--- php_vimgen.php
+++ php_vimgen.php
@@ -41,7 +41,7 @@ $extensions = array(
 	'xmlrpc', 'xsl', /*'wddx',*/ 'zip', 'zlib'
 	);
 
-$out_file = '/home/dqminh/.vim/php.vim'; // Pick your output file & location.
+$out_file = '/home/dqminh//.vim/php_vimgen_out.vim'; // Pick your output file & location.
 $out_str = '';
 $store = array();
 $errors = array();
